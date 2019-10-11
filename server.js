633280d607adec0e2b8df18b653d4dcec17f75e9
yeen <tibet_17931@hotmail.com>
2019-10-11 18:26:51 +0700

    Initial project2

--- server.js
+++ server.js
@@ -5,30 +5,18 @@ const app = express()
 const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    console.log(req.body.events[0])
-    reply(reply_token)
-    res.sendStatus(200)
-})
-app.listen(port)
 
-function reply(reply_token) {
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {Z+anN3HVX76QuBOs9N/RElWiihu0j66iEXFgGIyr9rEI2LMMkviS1JfilzgZj/qBqv/xErRQ64Jy4EADFHDSTbgfYSonU68ILNFuZCOR1qo2hwaNShpCu5X3JKpKwIdFgboGaCKArF6FU2jk8YLVtQdB04t89/1O/w1cDnyilFU=}'
+        'Authorization': 'Bearer {zaClGiD+ZEgcNeom4G6Vl/Acd1B6IjNnSwGJC5bDdl4ZjPxC00V9iTkqNRswf52Mqv/xErRQ64Jy4EADFHDSTbgfYSonU68ILNFuZCOR1qrp/cFE1qEjYvJNP9sYLksUcZjfL5lPI/TSM0/JvP1+nQdB04t89/1O/w1cDnyilFU=}'
     }
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
-                type: 'text',
-                text: 'Hello'
-            },
-            {
-                type: 'text',
-                text: 'How are you?'
-            }
-        ]
+            type: 'text',
+            text: msg
+        }]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
@@ -38,3 +26,14 @@ function reply(reply_token) {
         console.log('status = ' + res.statusCode);
     });
 }
+
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200)
+})
+
+app.listen(port, () => {
+    console.log("Express Listening at http://localhost:" + port);
+});
