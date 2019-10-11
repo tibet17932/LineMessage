0e5f0f2aeaf647a608703a98997938174f2216d8
yeen <tibet_17931@hotmail.com>
2019-10-11 17:59:26 +0700

    Initial project

--- server.js
+++ server.js
@@ -3,21 +3,31 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
 
-function reply(reply_token, msg) {
+function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {qAVj2qs962bjNUi/E+OL0jCSCga9/SqZ+t1ut91/TgNjiHVGRlT+w3cz0wz6QADyqv/xErRQ64Jy4EADFHDSTbgfYSonU68ILNFuZCOR1qozv9Fse4ooawDZBGDCLLQnBax6O3JS8/ZIXtJHUMudsAdB04t89/1O/w1cDnyilFU=}'
+        'Authorization': 'Bearer {xxxxxxx}'
     }
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
-            type: 'text',
-            text: msg
-        }]
+                type: 'text',
+                text: 'Hello'
+            },
+            {
+                type: 'text',
+                text: 'How are you?'
+            }
+        ]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
@@ -27,17 +37,3 @@ function reply(reply_token, msg) {
         console.log('status = ' + res.statusCode);
     });
 }
-
-app.post('/webhook', (req, res) => {
-    // let reply_token = req.body.replyToken
-    // let msg = req.body.msg
-    let reply_token = req.body.events[0].replyToken
-    let msg = req.body.events[0].msg
-    reply(reply_token, msg)
-    res.send(msg);
-})
-
-
-app.listen(port, () => {
-    console.log("Express Listening at http://localhost:" + port);
-});
