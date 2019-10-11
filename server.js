9a6199a5e3b50c7d87043ba4edd0bb223a5ea9c9
yeen <tibet_17931@hotmail.com>
2019-10-11 16:29:09 +0700

    Initial project

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.post('/webhook', (req, res) => {
+app.get('/webhook', (req, res) => {
     // let reply_token = req.body.events[0].replyToken
     // reply(reply_token)
     res.sendStatus(200)
