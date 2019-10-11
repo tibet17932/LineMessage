2b9a8bbaec5cf0d788187eed4ef38cfa8ad0fefd
yeen <tibet_17931@hotmail.com>
2019-10-11 17:04:02 +0700

    Initial project

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 function reply(reply_token) {
     let headers = {
